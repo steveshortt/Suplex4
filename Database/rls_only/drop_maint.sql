bb35de428638d090349d47f0bf5907a4c3cf142d
steveshortt <steve.shortt@gmail.com>
2016-10-29 11:54:15 -0500

    BitLib: a few touch-ups for dropping/re-creating db objects to lib & method signature changes

---
+++ Database/rls_only/drop_maint.sql
@@ -0,0 +1,4 @@
+drop PROCEDURE [splx].[splx_dal_sel_security_byuserbyuie]
+drop PROCEDURE [splx].[splx_dal_sel_userbyname]
+drop PROCEDURE [splx].[splx_dal_sel_userbyid]
+drop FUNCTION [SPLX].[Splx_GetTableOr]
